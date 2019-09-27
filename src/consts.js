8804d02c80f847a64056056cd601229ebaeb0ce3
Gregori Spielmann <gregorispielmann@MacBook-Pro-de-Gregori.local>
2019-09-27 17:54:51 -0300

    correção links local

--- src/consts.js
+++ src/consts.js
@@ -1,6 +1,6 @@
 export default {
-    // API_URL: 'https://gregoridev-mymoneyapp-back.herokuapp.com/api',
-    // OAPI_URL: 'https://gregoridev-mymoneyapp-back.herokuapp.com/oapi'
-    API_URL: 'http://localhost:3001/api',
-    OAPI_URL: 'http://localhost:3001/oapi'
+    API_URL: 'https://gregoridev-mymoneyapp-back.herokuapp.com/api',
+    OAPI_URL: 'https://gregoridev-mymoneyapp-back.herokuapp.com/oapi'
+    // API_URL: 'http://localhost:3001/api',
+    // OAPI_URL: 'http://localhost:3001/oapi'
 }
