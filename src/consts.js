5012ef44886b6f74b8b610a23259542de2827851
Gregori Spielmann <gregorispielmann@MacBook-Pro-de-Gregori.local>
2019-09-27 17:52:02 -0300

    correção uso local

--- src/consts.js
+++ src/consts.js
@@ -1,6 +1,6 @@
 export default {
     // API_URL: 'https://gregoridev-mymoneyapp-back.herokuapp.com/api',
     // OAPI_URL: 'https://gregoridev-mymoneyapp-back.herokuapp.com/oapi'
-    API_URL: 'http://localhost:8080/api',
-    OAPI_URL: 'http://localhost:8080/oapi'
+    API_URL: 'http://localhost:3001/api',
+    OAPI_URL: 'http://localhost:3001/oapi'
 }
