bb0653dc8cd0f795389dc827ddacc5b0a2bdd1a9
Gregori Spielmann <gregorispielmann@MacBook-Pro-de-Gregori.local>
2019-10-08 10:29:09 -0300

    build

---
+++ build/precache-manifest.f1be1215cd55cd8530cb3dc2150d52fd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a2c9ec35726412fd19442df54914f24",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ba73010deb2da9bff172",
+    "url": "/static/css/2.19b13f22.chunk.css"
+  },
+  {
+    "revision": "e3d01e0199ac3998e9ba",
+    "url": "/static/css/main.46436ebb.chunk.css"
+  },
+  {
+    "revision": "ba73010deb2da9bff172",
+    "url": "/static/js/2.be81c6b8.chunk.js"
+  },
+  {
+    "revision": "e3d01e0199ac3998e9ba",
+    "url": "/static/js/main.833fd8b1.chunk.js"
+  },
+  {
+    "revision": "219707186523ad4b8cde",
+    "url": "/static/js/runtime~main.821e3928.js"
+  }
+]);
